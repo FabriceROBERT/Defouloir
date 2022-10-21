409cca3311aa8207b94c63659c07836d0a56dddf
Fabriciooo <fabricerob@yahoo.fr>
2022-10-22 01:22:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const score =document.getElementById('score');
 const days = document.getElementById('days');
 const endScreen = document.getElementById('endScreen');
 
+ virusPop();
+
 function virusPop() {
     let virus = new Image();
 
+    // virus.src ="./User/fabri/OneDrive/Documents/Vscode/JS-click-game-starting_file/media/basic-pics/pngwave.png";
     virus.src ="./media/basic-pics/pngwave.png";
 
-    virus.classList.add('.virus');
-    virus.style.top = Math.random() * 500 + 'px';
-    virus.style.left = Math.random() * 500 + 'px';
+    virus.classList.add('virus');
+    virus.style.top = Math.random() * 800 + 'px';
+    virus.style.left = Math.random() * 800 + 'px';
     
     let x,y;
     x=y= (Math.random() * 45) + 30;
@@ -18,4 +21,5 @@ function virusPop() {
     virus.style.setProperty('--y', `${y}px`);
 
     canvas.appendChild(virus);
+    console.log(x);
 }
